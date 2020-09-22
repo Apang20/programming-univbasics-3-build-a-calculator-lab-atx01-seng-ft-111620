5e89ef30c1602e4791f3cc682188166b7d54d0c1
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 03:10:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,13 +14,16 @@ div
 end
 
 def multiplication(num1, num2)
-
+mult = num1 * num2
+mult
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
+mod
 end
 
 def square_root(num)
-
+total = Math.sqrt(num)
+total
 end
